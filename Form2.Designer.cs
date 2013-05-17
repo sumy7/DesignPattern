10de79831174f1e23f9dba5abbd33dc9c7401140
SUNMJ <sunmingjian7@qq.com>
2013-05-17 15:59:28 +0800

    新增上帝模式

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,15 +30,26 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 71);
+            this.button1.Location = new System.Drawing.Point(585, 9);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 38);
+            this.button1.Size = new System.Drawing.Size(93, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "添加UML图片";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -46,15 +57,126 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "设计模式名称";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(92, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(228, 23);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "设计模式简介";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(92, 40);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(227, 93);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "设计模式";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(92, 144);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(230, 131);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "更新简介";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 26);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "更新总结";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(338, 40);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(233, 235);
+            this.richTextBox4.TabIndex = 9;
+            this.richTextBox4.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "示例代码";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 24);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "更新示例代码";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(707, 287);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "GodMode";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,5 +184,16 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -30,18 +30,69 @@ namespace DesignPattern
                 imagebytes = br.ReadBytes(Convert.ToInt32(fs.Length));
                 //string strpath = Application.StartupPath + "\\a.xls";
                 //string strpath = Application.StartupPath + "\\b.mdb";
-                string strpath = "c:\\datebase.mdb";
+                string path = Application.StartupPath;
+                string strpath = path + @"/datebase.mdb";
                 string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + strpath;
                 OleDbConnection olecon = new OleDbConnection(constr);
                 olecon.Open();
                 //OleDbCommand oledcom = new OleDbCommand("insert into dezignpattern(UML) values(@ImageList)", olecon);
-                OleDbCommand oledcom = new OleDbCommand("update dezignpattern set UML=@ImageList where Name='简单工厂模式'", olecon);
+                OleDbCommand oledcom = new OleDbCommand("update dezignpattern set UML=@ImageList where Name='"+richTextBox1.Text+"'", olecon);
                 oledcom.Parameters.Add("ImageList", OleDbType.Binary);
                 oledcom.Parameters["ImageList"].Value = imagebytes;
                 oledcom.ExecuteNonQuery();
                 olecon.Close();
                 MessageBox.Show("图片保存完毕");
             }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //更新简介
+            string path = Application.StartupPath;
+            string strpath = path + @"/datebase.mdb";
+            string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + strpath;
+            OleDbConnection olecon = new OleDbConnection(constr);
+            olecon.Open();
+            //OleDbCommand oledcom = new OleDbCommand("insert into dezignpattern(UML) values(@ImageList)", olecon);
+            OleDbCommand oledcom = new OleDbCommand("update dezignpattern set Summer='"+richTextBox2.Text+"' where Name='" + richTextBox1.Text + "'", olecon);
+            oledcom.ExecuteNonQuery();
+            olecon.Close();
+            MessageBox.Show("简介更新完毕");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //更新总结
+            string path = Application.StartupPath;
+            string strpath = path + @"/datebase.mdb";
+            string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + strpath;
+            OleDbConnection olecon = new OleDbConnection(constr);
+            olecon.Open();
+            //OleDbCommand oledcom = new OleDbCommand("insert into dezignpattern(UML) values(@ImageList)", olecon);
+            OleDbCommand oledcom = new OleDbCommand("update dezignpattern set MainText='" + richTextBox2.Text + "' where Name='" + richTextBox1.Text + "'", olecon);
+            oledcom.ExecuteNonQuery();
+            olecon.Close();
+            MessageBox.Show("总结更新完毕");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //更新代码
+            string path = Application.StartupPath;
+            string strpath = path + @"/datebase.mdb";
+            string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + strpath;
+            OleDbConnection olecon = new OleDbConnection(constr);
+            olecon.Open();
+            //OleDbCommand oledcom = new OleDbCommand("insert into dezignpattern(UML) values(@ImageList)", olecon);
+            OleDbCommand oledcom = new OleDbCommand("update dezignpattern set Code='" + richTextBox2.Text + "' where Name='" + richTextBox1.Text + "'", olecon);
+            oledcom.ExecuteNonQuery();
+            olecon.Close();
+            MessageBox.Show("代码更新完毕");
         }
     }
 }
