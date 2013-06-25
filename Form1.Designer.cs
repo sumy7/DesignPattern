7e877c2a7860c062c2b35c5c2aa6e02bfa1deb0b
SUNMJ <sunmingjian7@qq.com>
2013-06-25 23:59:28 +0800

    删除无用代码，用户界面更新

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,16 +30,26 @@
         {
             this.treeView = new System.Windows.Forms.TreeView();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textSummer = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.picUML = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textMain = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.outpanel = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.picUML = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textMain = new System.Windows.Forms.RichTextBox();
+            this.buttons4 = new System.Windows.Forms.Button();
+            this.buttons2 = new System.Windows.Forms.Button();
+            this.buttons3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textSummer = new System.Windows.Forms.RichTextBox();
+            this.buttons1 = new System.Windows.Forms.Button();
             this.textCode = new DesignPattern.HighLightTextbox();
+            this.outpanel.SuspendLayout();
+            this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUML)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView
@@ -60,71 +70,10 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "设计模式";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(262, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "简介";
-            // 
-            // textSummer
-            // 
-            this.textSummer.BackColor = System.Drawing.Color.White;
-            this.textSummer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textSummer.ForeColor = System.Drawing.Color.Black;
-            this.textSummer.Location = new System.Drawing.Point(264, 45);
-            this.textSummer.Name = "textSummer";
-            this.textSummer.ReadOnly = true;
-            this.textSummer.Size = new System.Drawing.Size(511, 133);
-            this.textSummer.TabIndex = 4;
-            this.textSummer.Text = "";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(1118, 22);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "UML框图";
-            // 
-            // picUML
-            // 
-            this.picUML.Location = new System.Drawing.Point(967, 51);
-            this.picUML.Name = "picUML";
-            this.picUML.Size = new System.Drawing.Size(305, 425);
-            this.picUML.TabIndex = 6;
-            this.picUML.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(262, 190);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "模式说明";
-            // 
-            // textMain
-            // 
-            this.textMain.BackColor = System.Drawing.Color.White;
-            this.textMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textMain.Location = new System.Drawing.Point(264, 205);
-            this.textMain.Name = "textMain";
-            this.textMain.ReadOnly = true;
-            this.textMain.Size = new System.Drawing.Size(511, 329);
-            this.textMain.TabIndex = 8;
-            this.textMain.Text = "";
-            // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(1004, 12);
+            this.button1.Location = new System.Drawing.Point(12, 553);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 33);
             this.button1.TabIndex = 9;
@@ -132,46 +81,166 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label5
+            // outpanel
             // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(796, 33);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 12);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "示例代码";
+            this.outpanel.Controls.Add(this.panel4);
+            this.outpanel.Controls.Add(this.panel3);
+            this.outpanel.Controls.Add(this.panel2);
+            this.outpanel.Controls.Add(this.buttons4);
+            this.outpanel.Controls.Add(this.buttons2);
+            this.outpanel.Controls.Add(this.buttons3);
+            this.outpanel.Controls.Add(this.panel1);
+            this.outpanel.Controls.Add(this.buttons1);
+            this.outpanel.Location = new System.Drawing.Point(239, 45);
+            this.outpanel.Name = "outpanel";
+            this.outpanel.Size = new System.Drawing.Size(482, 489);
+            this.outpanel.TabIndex = 17;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.picUML);
+            this.panel4.Location = new System.Drawing.Point(374, 148);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(105, 148);
+            this.panel4.TabIndex = 32;
+            // 
+            // picUML
+            // 
+            this.picUML.Location = new System.Drawing.Point(3, 3);
+            this.picUML.Name = "picUML";
+            this.picUML.Size = new System.Drawing.Size(88, 105);
+            this.picUML.TabIndex = 7;
+            this.picUML.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textCode);
+            this.panel3.Location = new System.Drawing.Point(252, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(106, 148);
+            this.panel3.TabIndex = 31;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textMain);
+            this.panel2.Location = new System.Drawing.Point(363, 7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(108, 111);
+            this.panel2.TabIndex = 30;
+            // 
+            // textMain
+            // 
+            this.textMain.BackColor = System.Drawing.Color.White;
+            this.textMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textMain.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textMain.Location = new System.Drawing.Point(3, 3);
+            this.textMain.Name = "textMain";
+            this.textMain.ReadOnly = true;
+            this.textMain.Size = new System.Drawing.Size(86, 94);
+            this.textMain.TabIndex = 9;
+            this.textMain.Text = "";
+            // 
+            // buttons4
+            // 
+            this.buttons4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttons4.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttons4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttons4.Location = new System.Drawing.Point(173, 7);
+            this.buttons4.Name = "buttons4";
+            this.buttons4.Size = new System.Drawing.Size(48, 486);
+            this.buttons4.TabIndex = 29;
+            this.buttons4.Text = "UML\r\n框\r\n图";
+            this.buttons4.UseVisualStyleBackColor = false;
+            this.buttons4.Click += new System.EventHandler(this.buttons4_Click);
+            // 
+            // buttons2
+            // 
+            this.buttons2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttons2.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttons2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttons2.Location = new System.Drawing.Point(65, 7);
+            this.buttons2.Name = "buttons2";
+            this.buttons2.Size = new System.Drawing.Size(48, 486);
+            this.buttons2.TabIndex = 26;
+            this.buttons2.Text = "模\r\n式\r\n说\r\n明";
+            this.buttons2.UseVisualStyleBackColor = false;
+            this.buttons2.Click += new System.EventHandler(this.buttons2_Click);
+            // 
+            // buttons3
+            // 
+            this.buttons3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttons3.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttons3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttons3.Location = new System.Drawing.Point(119, 7);
+            this.buttons3.Name = "buttons3";
+            this.buttons3.Size = new System.Drawing.Size(48, 486);
+            this.buttons3.TabIndex = 27;
+            this.buttons3.Text = "示\r\n例\r\n代\r\n码";
+            this.buttons3.UseVisualStyleBackColor = false;
+            this.buttons3.Click += new System.EventHandler(this.buttons3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textSummer);
+            this.panel1.Location = new System.Drawing.Point(252, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(104, 108);
+            this.panel1.TabIndex = 25;
+            // 
+            // textSummer
+            // 
+            this.textSummer.BackColor = System.Drawing.Color.White;
+            this.textSummer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textSummer.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textSummer.ForeColor = System.Drawing.Color.Black;
+            this.textSummer.Location = new System.Drawing.Point(3, 8);
+            this.textSummer.Name = "textSummer";
+            this.textSummer.ReadOnly = true;
+            this.textSummer.Size = new System.Drawing.Size(91, 86);
+            this.textSummer.TabIndex = 5;
+            this.textSummer.Text = "";
+            // 
+            // buttons1
+            // 
+            this.buttons1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttons1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttons1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttons1.Location = new System.Drawing.Point(11, 7);
+            this.buttons1.Name = "buttons1";
+            this.buttons1.Size = new System.Drawing.Size(48, 486);
+            this.buttons1.TabIndex = 28;
+            this.buttons1.Text = "简\r\n介";
+            this.buttons1.UseVisualStyleBackColor = false;
+            this.buttons1.Click += new System.EventHandler(this.buttons1_Click);
             // 
             // textCode
             // 
             this.textCode.CodeType = null;
-            this.textCode.Location = new System.Drawing.Point(781, 51);
+            this.textCode.Location = new System.Drawing.Point(5, 16);
             this.textCode.Name = "textCode";
-            this.textCode.Size = new System.Drawing.Size(180, 486);
-            this.textCode.TabIndex = 11;
-            this.textCode.Load += new System.EventHandler(this.highLightTextbox1_Load);
+            this.textCode.Size = new System.Drawing.Size(91, 116);
+            this.textCode.TabIndex = 12;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1284, 578);
-            this.Controls.Add(this.textCode);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(983, 598);
+            this.Controls.Add(this.outpanel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textMain);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.picUML);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textSummer);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.treeView);
             this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.Name = "frmMain";
             this.Text = "设计模式";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.SizeChanged += new System.EventHandler(this.frmMain_SizeChanged);
+            this.outpanel.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picUML)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,15 +250,20 @@
 
         private System.Windows.Forms.TreeView treeView;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox textSummer;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox picUML;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox textMain;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel outpanel;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox picUML;
+        private System.Windows.Forms.Panel panel3;
         private HighLightTextbox textCode;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox textMain;
+        private System.Windows.Forms.Button buttons4;
+        private System.Windows.Forms.Button buttons2;
+        private System.Windows.Forms.Button buttons3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox textSummer;
+        private System.Windows.Forms.Button buttons1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,48 +13,34 @@ namespace DesignPattern
 
     public partial class frmMain : Form
     {
-        String dbLocation = null;
-        String path = null;
-        connDatebase Datebase;
+        private int slideBarCode = 0;
+        DatabaseFactory DataFactory = new AccessDBFactory();
+        connDatebase Database;
         public frmMain()
         {
+            Database = DataFactory.getFactory();
             InitializeComponent();
+            freshFrm();
         }
         private TreeNode treeListInit()
         {
-            List< KeyValuePair<string,int> > nodelist = Datebase.getLevel();
-            TreeNode[] node = new TreeNode[nodelist.Count()+1];
+            List<KeyValuePair<string, int>> nodelist = Database.getLevel();
+            TreeNode[] node = new TreeNode[nodelist.Count() + 1];
             node[0] = new TreeNode("设计模式");
-            Debug.WriteLine("--" + nodelist.Count());
-            //node[0].Text = "设计模式";
             for (int i = 1; i <= nodelist.Count(); i++)
-            //for(int i=nodelist.Count();i>0;i--)
             {
                 node[i] = new TreeNode(nodelist[i - 1].Key);
-                Debug.WriteLine("--" + nodelist[i - 1].Key);
                 node[nodelist[i - 1].Value].Nodes.Add(node[i]);
             }
-            //now.Nodes.Add(node[0]);
             return node[0];
         }
         private void frmMain_Load(object sender, EventArgs e)
         {
-            path = Application.StartupPath;
-            dbLocation = path + @"/datebase.mdb";
-            Datebase = new AccessDB(dbLocation);
-            if (!Datebase.connection())
-            {
-                MessageBox.Show("未找到数据库，请检查数据库位置。\n期望："+dbLocation);
-                Application.Exit();
-            }
-            //TreeNode node = new TreeNode("设计模式");
+            Database = new AccessDB("");
             treeView.Nodes.Add(treeListInit());
             treeView.TopNode.ExpandAll();
-            //textSummer.Text = (string)Datebase.getSummer("简单工厂模式");
-            //textMain.Text = (string)Datebase.getText("简单工厂模式");
-            //picUML.Image = Datebase.getUML("简单工厂模式");
             textCode.CodeType = "cpp";
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -67,22 +53,202 @@ namespace DesignPattern
         {
             if (treeView.SelectedNode != null)
             {
-                //MessageBox.Show("选中了：" + treeView.SelectedNode.Text);
-                textSummer.Text = (string)Datebase.getSummer(treeView.SelectedNode.Text);
-                textMain.Text = (string)Datebase.getText(treeView.SelectedNode.Text);
-                picUML.Image = Datebase.getUML(treeView.SelectedNode.Text);
-                textCode.Text = Datebase.getCode(treeView.SelectedNode.Text);
+                Database.execQuery(treeView.SelectedNode.Text);
+                textSummer.Text = (string)Database.getSummer();
+                textMain.Text = (string)Database.getText();
+                picUML.Image = Database.getUML();
+                textCode.Text = Database.getCode();
             }
         }
 
-        private void highLightTextbox1_Load(object sender, EventArgs e)
+        private void textCode_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void freshFrm()
         {
+            const int pad=20;
 
+            button1.Top = this.Height - button1.Height - 2*pad;
+            treeView.Height = button1.Top - treeView.Top ;
+
+            outpanel.Left = treeView.Left + treeView.Width;
+            outpanel.Top = treeView.Top;
+            outpanel.Width = this.Width - outpanel.Left-pad;
+            outpanel.Height = treeView.Height;
+
+            buttons1.Left = 0;
+            buttons1.Top = 0;
+            buttons1.Height = outpanel.Height;
+            buttons2.Left = buttons1.Left + buttons1.Width;
+            buttons2.Top = 0;
+            buttons2.Height = outpanel.Height;
+            buttons3.Left = buttons2.Left + buttons2.Width;
+            buttons3.Top = 0;
+            buttons3.Height = outpanel.Height;
+            buttons4.Left = buttons3.Left + buttons3.Width;
+            buttons4.Top = 0;
+            buttons4.Height = outpanel.Height;
+
+            switch (slideBarCode)
+            {
+                case 0: showpanel1(); break;
+                case 1: showpanel2(); break;
+                case 2: showpanel3(); break;
+                case 3: showpanel4(); break;
+                default: break;
+            }
         }
+        private void showpanel1()
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+
+            buttons1.Left = 0;
+            buttons1.Top = 0;
+            buttons1.Height = outpanel.Height;
+            buttons4.Left = outpanel.Width-buttons4.Width;
+            buttons4.Top = 0;
+            buttons4.Height = outpanel.Height;
+            buttons3.Left = buttons4.Left - buttons3.Width;
+            buttons3.Top = 0;
+            buttons3.Height = outpanel.Height;
+            buttons2.Left = buttons3.Left - buttons2.Width;
+            buttons2.Top = 0;
+            buttons2.Height = outpanel.Height;
+
+            panel1.Left = buttons1.Left + buttons1.Width;
+            panel1.Top = 0;
+            panel1.Width = outpanel.Width - 4 * buttons1.Width;
+            panel1.Height = outpanel.Height;
+
+            textSummer.Top = 0;
+            textSummer.Left = 0;
+            textSummer.Width = panel1.Width;
+            textSummer.Height = panel1.Height;
+        }
+        private void showpanel2()
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            panel3.Visible = false;
+            panel4.Visible = false;
+
+            buttons1.Left = 0;
+            buttons1.Top = 0;
+            buttons1.Height = outpanel.Height;
+            buttons2.Left = buttons1.Left + buttons1.Width;
+            buttons2.Top = 0;
+            buttons2.Height = outpanel.Height;
+            buttons4.Left = outpanel.Width - buttons4.Width;
+            buttons4.Top = 0;
+            buttons4.Height = outpanel.Height;
+            buttons3.Left = buttons4.Left - buttons3.Width;
+            buttons3.Top = 0;
+            buttons3.Height = outpanel.Height;
+
+            panel2.Left = buttons2.Left + buttons2.Width;
+            panel2.Top = 0;
+            panel2.Width = outpanel.Width - 4 * buttons1.Width;
+            panel2.Height = outpanel.Height;
+
+            textMain.Top = 0;
+            textMain.Left = 0;
+            textMain.Width = panel2.Width;
+            textMain.Height = panel2.Height;
+        }
+        private void showpanel3()
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = true;
+            panel4.Visible = false;
+
+            buttons1.Left = 0;
+            buttons1.Top = 0;
+            buttons1.Height = outpanel.Height;
+            buttons2.Left = buttons1.Left + buttons1.Width;
+            buttons2.Top = 0;
+            buttons2.Height = outpanel.Height;
+            buttons3.Left = buttons2.Left + buttons2.Width;
+            buttons3.Top = 0;
+            buttons3.Height = outpanel.Height;
+            buttons4.Left = outpanel.Width - buttons4.Width;
+            buttons4.Top = 0;
+            buttons4.Height = outpanel.Height;
+
+            panel3.Left = buttons3.Left + buttons3.Width;
+            panel3.Top = 0;
+            panel3.Width = outpanel.Width - 4 * buttons1.Width;
+            panel3.Height = outpanel.Height;
+
+            textCode.Top = 0;
+            textCode.Left = 0;
+            textCode.Width = panel3.Width;
+            textCode.Height = panel3.Height;
+        }
+        private void showpanel4()
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = true;
+
+            buttons1.Left = 0;
+            buttons1.Top = 0;
+            buttons1.Height = outpanel.Height;
+            buttons2.Left = buttons1.Left + buttons1.Width;
+            buttons2.Top = 0;
+            buttons2.Height = outpanel.Height;
+            buttons3.Left = buttons2.Left + buttons2.Width;
+            buttons3.Top = 0;
+            buttons3.Height = outpanel.Height;
+            buttons4.Left = buttons3.Left + buttons3.Width;
+            buttons4.Top = 0;
+            buttons4.Height = outpanel.Height;
+
+            panel4.Left = buttons4.Left + buttons4.Width;
+            panel4.Top = 0;
+            panel4.Width = outpanel.Width - 4 * buttons1.Width;
+            panel4.Height = outpanel.Height;
+
+            picUML.Top = 0;
+            picUML.Left = 0;
+            picUML.Width = panel4.Width;
+            picUML.Height = panel4.Height;
+        }
+
+        private void buttons1_Click(object sender, EventArgs e)
+        {
+            showpanel1();
+            slideBarCode = 0;
+        }
+
+        private void buttons2_Click(object sender, EventArgs e)
+        {
+            showpanel2();
+            slideBarCode = 1;
+        }
+
+        private void buttons3_Click(object sender, EventArgs e)
+        {
+            showpanel3();
+            slideBarCode = 2;
+        }
+
+        private void buttons4_Click(object sender, EventArgs e)
+        {
+            showpanel4();
+            slideBarCode = 3;
+        }
+
+        private void frmMain_SizeChanged(object sender, EventArgs e)
+        {
+            freshFrm();
+        }
+
     }
 }
